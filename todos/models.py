2d5645ebc772463347b01b6782bb25205c93495a
Abyssinia Negede <abyssinianegede32@gmail.com>
2023-07-15 15:48:25 +0300

    userprofile model added

--- todos/models.py
+++ todos/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    User_email  = models.EmailField(max_length=70,blank=True,unique=True)
+    password    = models.CharField(max_length=12)
+
+
+    def __str__(self):
+        return self.user.username
+    
+
 class Task(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE )
@@ -15,3 +26,5 @@ class Task(models.Model):
     
     class Meta:
         ordering = ['complete']
+
+
